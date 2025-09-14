22e922216a667bdfce2712e43e600e8c0bfbe2d5
felipeg17 <fegonzalez17@gmail.com>
2025-09-13 20:15:22 -0500

    added new attributes to pokemon class

--- pokemon.py
+++ pokemon.py
@@ -11,7 +11,10 @@ class Pokemon:
             self,
             name: str,
             pokedex_num: int,
-            type: str
+            type: str, 
+            color: str,
+            sex: str,
+            level: int = 1
         ) -> None:
         """
         Creates a basic pokemon
@@ -21,27 +24,38 @@ class Pokemon:
             pokedex_num (int): Number in the national pokedex
             type (str): Main type of the pokemon
         Returns:
-            
+           A string representation of the Pokemon.
 
         Raises:
-
+            ...
 
         """
 
         self.name = name
         self.pokedex_num = pokedex_num
         self.main_type = type
-
+        self.color = color
+        self.sex = sex
+        self.level = level
+    
 
     def attack(self):
         print(f"{self.name} is attacking!") 
 
-    
+    def level_up(self):
+        self.level += 1
+        print(f"{self.name} leveled up to level {self.level}!")
+
+    def __str__(self):
+        return f"{self.name} (#{self.pokedex_num}) - Type: {self.main_type}, Level: {self.level}"
+
+
 if __name__ == "__main__":
     bulbasaur = Pokemon(
         "bulbasaur",
         1,
         "grass"
     )
+    print(bulbasaur)
     bulbasaur.attack()
 
