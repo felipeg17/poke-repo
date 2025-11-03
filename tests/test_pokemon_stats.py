aee6b4ce5eea757d3fa7c28008ebdbd719853b5f
felipeg17 <fegonzalez17@gmail.com>
2025-11-02 19:21:38 -0500

    fix refactor tests

--- tests/test_pokemon_stats.py
+++ tests/test_pokemon_stats.py
@@ -1,36 +1,4 @@
-from pokemon import Pokemon, Grass, Fire
-
-
-def test_create_pokemon():
-    bulbasaur = Pokemon(
-        pokemon_name="bulbasaur",
-        type="Grass",
-        level=5,
-        pokedex_num=1,
-        color="green",
-        sex="male",
-    )
-    assert bulbasaur.get_attribute("pokemon_name") == "bulbasaur"
-    assert bulbasaur.get_attribute("type") == "Grass"
-    assert bulbasaur.get_attribute("level") == 5
-    assert bulbasaur.get_attribute("pokedex_num") == 1
-    assert bulbasaur.get_attribute("color") == "green"
-    assert bulbasaur.attack() == "bulbasaur is attacking!"
-
-
-def test_pokemon_resistances_and_weaknesses():
-    bulbasaur = Grass(
-        name="bulbasaur", level=5, pokedex_num=1, color="green", sex="male"
-    )
-    charmander = Fire(
-        name="charmander", level=5, pokedex_num=4, color="red", sex="male"
-    )
-    assert bulbasaur.receive_attack("Fire") == "It's super effective!"
-    assert bulbasaur.receive_attack("Water") == "It's not very effective..."
-    assert charmander.receive_attack("Water") == "It's super effective!"
-    assert charmander.receive_attack("Grass") == "It's not very effective..."
-    assert bulbasaur.receive_attack("Electric") == "It's not very effective..."
-    assert charmander.receive_attack("Normal") == "It's effective."
+from pokemon import Grass, Fire
 
 
 def test_pokemon_stats_base():
@@ -76,7 +44,3 @@ def test_level_up_method_increases_stats():
     assert round(stats_after.sp_attack) == 65
     assert round(stats_after.sp_defense) == 55
     assert round(stats_after.speed) == 70
-
-
-if __name__ == "__main__":
-    pass
